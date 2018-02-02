0365e76a8b0a90094ffa0925932397ac4f772522
Nam Nguyen <Nam Nguyen@NamNguyen-Notebook>
2018-02-02 15:51:17 +0100

    read data

--- SDM_Ex10_Prob1/src/de/tuda/sdm/dmdb/exercise10/prob1/Job.java
+++ SDM_Ex10_Prob1/src/de/tuda/sdm/dmdb/exercise10/prob1/Job.java
@@ -1,41 +1,54 @@
 package de.tuda.sdm.dmdb.exercise10.prob1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Job {
-    public static void main(String[] args) throws IOException {
-        run("visits_small.csv", "favorites_small.json");
-    }
+	public static void main(String[] args) throws IOException {
+		run("visits_small.csv", "favorites_small.json");
+	}
 
-    public static void run(String visitDatasetFileName, String outFileName) throws IOException
-    {
-        /*
-        Read all the data
-         */
+	static String DELIMETER = ";";
 
-        //Context<...> readerContext = new Context<>();
-        //Reader reader = ...;
-        //reader.read(..., ..., readerContext);
+	public static void run(String visitDatasetFileName, String outFileName) throws IOException {
+		// read visitData
+		String visitDataInput = "data/" + visitDatasetFileName;
+		Context<String, String> visitDataReaderContext = new Context<String, String>();
 
-        /*
-        Perform mapping and reducing steps (as many as needed)
-         */
+		File visitDataFile = new File(visitDataInput);
 
-        //Context<...> mapper1Context = new Context<>();
-        //Mapper<...> mapper1 = new ...;
-        //mapper1.run(readerContext, mapper1Context);
+		Reader<String, String> reader = new Reader<>();
+		reader.read(visitDataFile, DELIMETER, visitDataReaderContext);
 
-        //Context<...> reducer1Context = new Context<>();
-        //Reducer<...> reducer1 = new ...;
-        //reducer1.run(mapper1Context, reducer1Context);
+		// read devices.csv
+		Context<String, String> devicesReaderContext = new Context<String, String>();
+		String devicesData = "data/devices.csv";
 
-        //...
+		File devicesFile = new File(devicesData);
+		reader.read(devicesFile, DELIMETER, devicesReaderContext);
+		
+		/*
+		 * Perform mapping and reducing steps (as many as needed)
+		 */
 
-        /*
-        Write resulting records to file
-         */
+		// Context<...> mapper1Context = new Context<>();
+		// Mapper<...> mapper1 = new ...;
+		// mapper1.run(readerContext, mapper1Context);
 
-        //RecordWriter<String, List<String>> writer = new ...;
-        //writer.run(...);
-    }
+		// Context<...> reducer1Context = new Context<>();
+		// Reducer<...> reducer1 = new ...;
+		// reducer1.run(mapper1Context, reducer1Context);
+
+		// ...
+
+		/*
+		 * Write resulting records to file
+		 */
+
+		// RecordWriter<String, List<String>> writer = new ...;
+		// writer.run(...);
+	}
 }
